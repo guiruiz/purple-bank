df989d8c70630decd9251921132532275beb683d
Guilherme Ruiz <guiiruiz@hotmail.com>
2019-06-04 04:14:51 -0300

    Rename service handlers

--- src/purple_bank/service.clj
+++ src/purple_bank/service.clj
@@ -3,10 +3,10 @@
             [purple-bank.interceptor :as interceptor]
             [purple-bank.controller :as controller]))
 
-(defn handle-welcome-message [request]
+(defn welcome-message-handler [request]
     (ring-resp/response "Welcome to Purple Bank! Check README.md to get started."))
 
-(defn handle-create-user
+(defn create-user-handler
   "Tries to create a new user from request. If it's successful, returns response with status code 201,
   created user on body and a Location header containing user path. If it's unsuccessful, returns
   response with status code 400."
@@ -19,12 +19,12 @@
         (ring-resp/status 201))
     (ring-resp/status {} 400)))
 
-(defn handle-get-user
+(defn get-user-handler
   "Returns response with status code 200 and the user on its body."
   [{user :user}]
     (ring-resp/response user))
 
-(defn handle-create-transaction
+(defn create-transaction-handler
   "First, tries to build a transaction. If the transaction is valid, tries to process it.
   If the transaction process is successful, returns a response with status code 201,
   created transaction on body and a Location header containing transaction path.
@@ -43,12 +43,12 @@
     (ring-resp/status {} 400)))
 
 (def routes #{["/" :get (conj interceptor/common-interceptors
-                              `handle-welcome-message)]
+                              `welcome-message-handler)]
               ["/users" :post (conj interceptor/common-interceptors
-                                    `handle-create-user)]
+                                    `create-user-handler)]
               ["/users/:user-id" :get (conj interceptor/common-interceptors
                                             interceptor/validate-user-id
-                                            `handle-get-user)]
+                                            `get-user-handler)]
               ["/users/:user-id/transactions" :post (conj interceptor/common-interceptors
                                                           interceptor/validate-user-id
-                                                          `handle-create-transaction)]})
+                                                          `create-transaction-handler)]})
