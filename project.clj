cb6e0e1e9d971750ad0f441b88dd746e9cdfc361
Guilherme Ruiz <guiiruiz@hotmail.com>
2019-06-07 18:24:47 -0300

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject purple-bank "0.1.0-SNAPSHOT"
   :description "Purple Bank"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :url "http://nubank.com.br"
+  :license {:name "Free"}
+  :plugins [[lein-midje "3.2.1"]]
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [io.pedestal/pedestal.service "0.5.5"]
                  [io.pedestal/pedestal.jetty "0.5.5"]
